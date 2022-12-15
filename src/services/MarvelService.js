503b39e2e4e816219f0952b99b2d973639add8fa
Sixzer <iam6zer@gmail.com>
2022-12-15 19:14:43 +0300

    add methods to useMarvelService

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -17,7 +17,15 @@ const useMarvelService = () => {
         return _transformChar(res.data.results[0]);
     }
 
+    const getAllComics = async (offset = 0) => {
+        const res = await request(`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_transformComics);
+    }
 
+    const getSingleComics = async (id) => {
+        const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+        return _transformComics(res.data.results[0]);
+    }
 
     const _transformChar = (char) => {
 
@@ -40,7 +48,19 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, getAllCharacters, getSingleCharacter, clearError}
+    const _transformComics = (comics) => {
+        return {
+            id: comics.id,
+            title: comics.title,
+            description: comics.description || 'There is no description',
+            pageCount: comics.pageCount ? `${comics.pageCount} p.` : 'No information about the number of pages',
+            thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
+            language: comics.textObjects.language || 'en-us',
+            price: comics.prices.price ? `${comics.prices.price}$` : 'not available',
+        }
+    }
+
+    return {loading, error, getAllCharacters, getSingleCharacter, getAllComics, getSingleComics, clearError}
 }
 
 export default useMarvelService;
