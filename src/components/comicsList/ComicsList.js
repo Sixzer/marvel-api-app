55ffc11fa72c1ee8b4b5321e25ee23e3ccb4326b
Sixzer <iam6zer@gmail.com>
2022-12-16 15:40:18 +0300

    add ComicsList logic

--- src/components/comicsList/ComicsList.js
+++ src/components/comicsList/ComicsList.js
@@ -4,34 +4,72 @@ import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 
 import './comicsList.scss';
-import uw from '../../resources/img/UW.png';
-import xMen from '../../resources/img/x-men.png';
 
 const ComicsList = () => {
 
-
     const [comicsList, setComicsList] = useState([]);
     const [reloading, setReloading] = useState(false);
-    const [offset, setOffset] = useState(0);
+    const [offset, setOffset] = useState(1000);
     const [comicsEnded, setComicsEnded] = useState(false);
 
     const {loading, error, getAllComics} = useMarvelService();
 
-    return (
-        <div className="comics__list">
-            <ul className="comics__grid">
-                <li className="comics__item">
+    useEffect(() => {
+        onRequest(offset, true);
+    }, []);
+
+    const onRequest = (offset, initial) => {
+        initial ? setReloading(false) : setReloading(true);
+        getAllComics(offset).then(onComicsListLoaded);
+    }
+
+    const onComicsListLoaded = (newComicsList) => {
+        let ended = false;
+        if (newComicsList.length < 8) {
+            ended = true;
+        }
+        
+        setComicsList([...comicsList, ...newComicsList]);
+        setReloading(false);
+        setOffset(offset + 8);
+        setComicsEnded(ended);
+    }
+
+    const renderComics = function (arr) {
+        const comics = arr.map((item, i) => {
+            return (
+                <li className="comics__item" key={i}>
                     <a href="#">
-                        <img src={uw} alt="ultimate war" className="comics__item-img"/>
-                        <div className="comics__item-name">ULTIMATE X-MEN VOL. 5: ULTIMATE WAR TPB</div>
-                        <div className="comics__item-price">9.99$</div>
+                        <img src={item.thumbnail} alt={item.title} className="comics__item-img"/>
+                        <div className="comics__item-name">{item.title}</div>
+                        <div className="comics__item-price">{item.price}</div>
                     </a>
                 </li>
-            </ul>
+            )
+        })
 
+        return (
+            <ul className="comics__grid">
+                {comics}
+            </ul>
+        )
+    }
+
+    const comics = renderComics(comicsList);
+
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading && !reloading ? <Spinner/> : null;
+
+    return (
+        <div className="comics__list">
+            {errorMessage}
+            {spinner}
+            {comics}
             <button 
+                disabled={reloading} 
+                style={{'display' : comicsEnded ? 'none' : 'block'}}
                 className="button button__main button__long"
-                style={{}}>
+                onClick={() => onRequest(offset)}>
                 <div className="inner">load more</div>
             </button>
         </div>
